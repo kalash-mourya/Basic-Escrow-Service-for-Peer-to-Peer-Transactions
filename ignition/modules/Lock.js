07dd5eb4b7d65b52deac2df2823442c27e89ec3e
kalash mourya <135300823+kalash-mourya@users.noreply.github.com>
2025-07-19 22:05:44 +0530

    Update Lock.js

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -2,7 +2,6 @@
 // Learn more about it at https://hardhat.org/ignition
 
 const{ buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
 const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI = 1_000_000_000n;
 
